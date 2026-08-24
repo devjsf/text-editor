fdac4d59b3ac5418e041d40b975d4152aea9447d
devjsf <imjoshuasf@gmail.com>
2026-08-24 13:38:57 -0700

    improve error handling, change quit to ctrl+c, use direct KeyEvents

--- src/editor.rs
+++ src/editor.rs
@@ -1,24 +1,34 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
 
 pub struct Editor {
-
+    to_quit: bool,
 }
+
 impl Editor {
-    pub fn run() {
+    pub fn default() -> Self {
+        Editor {to_quit: false}
+    }
+
+    pub fn run(&mut self) {
+        if let Err(error) = self.repl() {
+            panic!("{error:#?}")
+        }
+    }
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode().unwrap();
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?}\r");
-                    if let Char(c) = event.code && c == 'q' {
-                            break;
-                    }
-                },
-                Err(error) => println!("Error {error}"),
-                _ => (),
+            if let Key(KeyEvent{code, modifiers, kind, state}) = read()? {
+                println!("{code:?} {modifiers:?} {kind:?} {state:?}\r");
+                if let Char('c') = code && modifiers == KeyModifiers::CONTROL {
+                        self.to_quit = true;
+                }
+            }
+            if self.to_quit {
+                break;
             }
         }
         disable_raw_mode().unwrap();
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    Editor::run();
+    Editor::default().run();
 }
